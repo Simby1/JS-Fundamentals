3043ece569030a107f2e5a89a63d1a9daf5fe469
Simby1 <kimsimby@gmail.com>
2025-09-14 20:54:56 +0100

    Add multi_languages_loop.js to display programming language messages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let result = "";
+for (let i = 0; i < languages.length; i++) {
+  result += languages[i] + "\n";
+}
+console.log(result.trim());
